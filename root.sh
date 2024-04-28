0e5aff210414054c80b7798855f741e70e6d18d9
FrostNovaHD <wesleyvanneck1012@gmail.com>
2024-04-28 11:36:59 +0200

    revert

--- root.sh
+++ root.sh
@@ -79,7 +79,7 @@ echo "[+] Pointing ${KERNEL_PATH}/vmlinux to vmlinux.ksu..."
 ln -s vmlinux.ksu ${KERNEL_PATH}/vmlinux
 
 echo "[+] Cleanup..."
-fusermount /tmp/ksu -o nonempty
+fusermount -u /tmp/ksu
 rmdir /tmp/ksu
 
 echo
